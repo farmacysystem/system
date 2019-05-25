644d3e23765f2b6c79bd1a6cb5e3d684537d350e
Chamod Lakmal <chamodlakmal97@gmail.com>
2019-05-25 22:56:45 +0530

    update

--- Farmacy/src/view/UpdateBrand.java
+++ Farmacy/src/view/UpdateBrand.java
@@ -74,8 +74,6 @@ public class UpdateBrand extends javax.swing.JPanel {
             }
         });
 
-        selectBrandName.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
